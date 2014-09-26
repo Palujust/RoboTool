a0ffecb8a438a643d11c863fc2789edb449ad18c
Zachary Yang <zach.yang.zy@gmail.com>
2014-09-26 16:03:33 -0400

    milestones now dynamically update in popup window

--- JS/popup.js
+++ JS/popup.js
@@ -1,16 +1,34 @@
 var milestones = ['Login Page URL', 'Login Page Regex', 'Invalid Creds Regex']
 
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+    if (areaName === "sync") {
+        for (var key in changes) {
+            var location = milestones.indexOf(key);
+            if (location !== -1) {
+                updateMilestone(location, changes[key].newValue);
+            }
+        }
+    }
+})
+
 $(document).ready(function() {
 
-    $(".clearButton").click(function() {
-        chrome.storage.sync.set({
-            'Login Page URL': 'hey',
-            'Login Page Regex': 'wow',
-            'Invalid Creds Regex': 'woawko'
-        })
+    $("#clearButton").click(function() {
+        chrome.storage.sync.clear();
+        for (var index in milestones) {
+            updateDeletedMilestone(index);
+        }
+    })
+
+    $("#setCustomKeys").click(function() {
+        var key = $("#key").val();
+        var value = $("#value").val();
+        var set_obj = {};
+        set_obj[key] = value;
+        chrome.storage.sync.set(set_obj);
     })
     for (var i = 0; i < milestones.length; i++) {
-        $("#info-table>tbody").append("<tr><td><img class = \"status-image\" src=\"Img/x.png\"></td>" +
+        $("#info-table>tbody").append("<tr><td><img class=\"status-image\" src=\"Img/x.png\"></td>" +
                                       "<td>" + milestones[i] + "</td>" +
                                       "<td id=status" + i + ">N/A</td></tr>");
     }
@@ -26,7 +44,7 @@ function retrieveProgress() {
         } else {
             for (var index in milestones) {
                 if (milestones[index] in results) {
-                    $("#status" + index).text(results[milestones[index]]);
+                    updateMilestone(index, results[milestones[index]]);
                 }
             }
         }
@@ -35,3 +53,17 @@ function retrieveProgress() {
 }
 
 window.onload = retrieveProgress;
+
+// If new_val is false, the milestone will be turned off
+function updateMilestone(index, new_val) {
+    if (new_val) {
+        $("#status" + index).text(new_val);
+        $(".status-image").eq(index).attr('src', 'Img/check.png');
+    } else {
+        $("#status" + index).text('N/A');
+        $(".status-image").eq(index).attr('src', 'Img/x.png');
+    }
+}
+function deleteDeletedMilestone(index) {
+    updateMilestone(index, null);
+}
