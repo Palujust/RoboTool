c15a3cb8b3891229e07d5f171b936ef3952ff36c
Zachary Yang <zach.yang.zy@gmail.com>
2014-09-26 15:27:25 -0400

    Popup window is now dynamically populated

--- JS/popup.js
+++ JS/popup.js
@@ -1,83 +1,37 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var milestones = ['Login Page URL', 'Login Page Regex', 'Invalid Creds Regex']
 
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'kittens';
+$(document).ready(function() {
 
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
-
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
-
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
+    $(".clearButton").click(function() {
+        chrome.storage.sync.set({
+            'Login Page URL': 'hey',
+            'Login Page Regex': 'wow',
+            'Invalid Creds Regex': 'woawko'
+        })
+    })
+    for (var i = 0; i < milestones.length; i++) {
+        $("#info-table>tbody").append("<tr><td><img class = \"status-image\" src=\"Img/x.png\"></td>" +
+                                      "<td>" + milestones[i] + "</td>" +
+                                      "<td id=status" + i + ">N/A</td></tr>");
     }
-  },
+})
 
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
-};
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
-});
+function retrieveProgress() {
+    chrome.storage.sync.get(milestones, function(results) {
+        if (chrome.extension.lastError) {
+            alert("No saved data");
+        } else if (!results) {
+            alert("No saved data 2");
+        } else {
+            for (var index in milestones) {
+                if (milestones[index] in results) {
+                    $("#status" + index).text(results[milestones[index]]);
+                }
+            }
+        }
+
+    })
+}
+
+window.onload = retrieveProgress;
